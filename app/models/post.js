50563fd1ef4f56d9597caebc3586cdac78362520
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2017-10-10 19:53:16 +0200

    add bootstrap 4 and start with blog list

---
+++ app/models/post.js
@@ -0,0 +1,8 @@
+import DS from 'ember-data';
+
+export default DS.Model.extend({
+    creator: DS.attr("string"),
+    date: DS.attr(),
+    content: DS.attr("string"),
+    title: DS.attr("string")
+});
---
+++ server/mocks/posts.js
@@ -0,0 +1,67 @@
+/* eslint-env node */
+'use strict';
+
+module.exports = function(app) {
+  const express = require('express');
+  let postsRouter = express.Router();
+
+  postsRouter.get('/', function(req, res) {
+    res.send({
+      'posts': [{
+          id: 1,
+          creator: "Sascha",
+          date: "01.08.2000",
+          content: "Blog post 1",
+          title: "Test 1"
+        },{
+          id: 2,
+          creator: "Joris",
+          date: "01.08.2000",
+          content: "Blog post 2",
+          title: "Test 2"
+        },{
+          id: 3,
+          creator: "Yves",
+          date: "01.08.2000",
+          content: "Blog post 3",
+          title: "Test 3"
+        }]
+    });
+  });
+
+  postsRouter.post('/', function(req, res) {
+    res.status(201).end();
+  });
+
+  postsRouter.get('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.put('/:id', function(req, res) {
+    res.send({
+      'posts': {
+        id: req.params.id
+      }
+    });
+  });
+
+  postsRouter.delete('/:id', function(req, res) {
+    res.status(204).end();
+  });
+
+  // The POST and PUT call will not contain a request body
+  // because the body-parser is not included by default.
+  // To use req.body, run:
+
+  //    npm install --save-dev body-parser
+
+  // After installing, you need to `use` the body-parser for
+  // this mock uncommenting the following line:
+  //
+  //app.use('/api/posts', require('body-parser').json());
+  app.use('/api/posts', postsRouter);
+};
