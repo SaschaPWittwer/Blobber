22d3f38210c250bea6f663e18a4fe51e85affa1a
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2017-12-19 19:01:24 +0100

    add npm lock

--- app/routes/application.js
+++ app/routes/application.js
@@ -2,6 +2,8 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        return this.get("store").findAll("blogpost");
+        return this.get("store").findAll("blogpost").then(result => {
+            return result.slice(0,10);
+        });
     }
 });
