ee5d65be0521f00c551beb59b7992745356d2514
SaschaPWittwer <sascha.wittwer@bluewin.ch>
2017-12-24 15:06:13 +0100

    Batches for liking/unliking of posts

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
         return this.get("store").findAll("blogpost").then(result => {
-            return result.slice(0,10);
+            return result.slice(0,8);
         });
     }
 });
